4ce51e512a8195a7e5c908cedfa74115497b7596
Milena Confeggi <milenaconfeggi@gmail.com>
2024-11-16 14:46:52 -0300

    ya se puede eliminar un mail

--- servicios/backend/src/web/controllers/mails.py
+++ servicios/backend/src/web/controllers/mails.py
@@ -29,8 +29,8 @@ def obtener_imagen(id_legajo, filename):
     print("File found, sending from directory")
     return send_from_directory(folder_path, filename)
 
-@bp.post("/subir_mail/<int:id>")
-def cargar_mail(id):
+@bp.post("/subir_mail/<int:id_legajo>")
+def cargar_mail(id_legajo):
     if 'archivo' not in request.files:
         return jsonify({"error": "Debes selecionar al menos una imagen"}), 400
 
@@ -40,18 +40,27 @@ def cargar_mail(id):
 
     for file in files:
         if file and servicioMail.validar_tipo(file.filename):
-            if(servicioMail.validar_nombre(file.filename, id) == False):
+            if(servicioMail.validar_nombre(file.filename, id_legajo) == False):
                 return jsonify({"error": "Ya existe un archivo con ese nombre para el legajo"}), 400
             filename = secure_filename(file.filename)
-            folder_path = os.path.join(UPLOAD_FOLDER, "mails", str(id))
+            folder_path = os.path.join(UPLOAD_FOLDER, "mails", str(id_legajo))
             os.makedirs(folder_path, exist_ok=True)
             file.save(os.path.join(folder_path, filename))
             data = {
                 'nombre_archivo': filename,
-                'legajo_id': id
+                'legajo_id': id_legajo
             }
-            servicioMail.crear_mail(data, id)
+            servicioMail.crear_mail(data, id_legajo)
         else:
             return jsonify({"error": "El tipo del archivo debe ser 'png', 'jpg' o 'jpeg' "}), 400
 
     return jsonify({"message": "Mails cargados correctamente"}), 200
+
+@bp.post("/eliminar_mail/<int:id_mail>")
+def eliminar_mail(id_mail):
+    mail = servicioMail.eliminar_mail(id_mail)
+    folder_path = os.path.join(UPLOAD_FOLDER, "mails", str(mail.legajo_id))
+    file_path = os.path.join(folder_path, mail.nombre_archivo)
+    if os.path.exists(file_path):
+        os.remove(file_path)
+    return jsonify({"message": "Mail eliminado correctamente"}), 200

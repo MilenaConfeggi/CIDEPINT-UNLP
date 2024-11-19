f0abb40947e8097cd8b25a8a9c724fbe493c7f90
Milena Confeggi <milenaconfeggi@gmail.com>
2024-11-19 16:55:48 -0300

    agrego tipo a documentos

--- models/documentos/documento.py
+++ models/documentos/documento.py
@@ -5,5 +5,11 @@ class Documento(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre_documento = db.Column(db.String(255), nullable=False)
     fecha_creacion = db.Column(db.DateTime, nullable=False)
-    estado_id = db.Column(db.Integer, db.ForeignKey('estado.id'), nullable=False)
+
+    estado_id = db.Column(db.Integer, db.ForeignKey('estado.id'), nullable=True) #nullable=True porque el documento puede no tener estado (presupuesto conicet)
+    estado = db.relationship('Estado', backref=db.backref('documento', uselist=False))
     legajo_id = db.Column(db.Integer, db.ForeignKey('legajo.id'), nullable=False)
+    legajo = db.relationship('Legajo', backref=db.backref('documento', uselist=False))
+    #Agrego el tipo de documento (presupuesto, informe, certificado, etc)
+    tipo_documento_id = db.Column(db.Integer, db.ForeignKey('tipo_documento.id'), nullable=False)
+    tipo_documento = db.relationship('Tipo_Documento', backref=db.backref('documento', uselist=False))
--- servicios/backend/src/core/seeds/seedsInforme.py
+++ servicios/backend/src/core/seeds/seedsInforme.py
@@ -1,7 +1,8 @@
-from servicios.backend.src.core.services.servicioInforme import crear_documento, crear_estado
+from servicios.backend.src.core.services.servicioInforme import crear_documento, crear_estado, crear_tipo_documento
 
 def seeds_informe():
     seed_estados()
+    seed_tipo()
     seed_documentos()
 
 def seed_estados():
@@ -25,32 +26,67 @@ def seed_estados():
     for data in estados_data:
         crear_estado(data['nombre'])
 
+def seed_tipo():
+    #Probalemente haya que agregar
+    tipos_data = [
+        {
+            'nombre': 'informe'
+        },
+        {
+            'nombre': 'certificado'
+        },
+        {
+            'nombre': 'presupuestoCi'
+        },
+        {
+            'nombre': "presupuestoCo"
+        },
+        {
+            'nombre': "ordenCompra"
+        },
+        {
+            'nombre': "comrpobantePago"
+        },
+        {
+            'nombre': "factura"
+        },
+
+    ]
+
+    for data in tipos_data:
+        crear_tipo_documento(data['nombre'])
+
 def seed_documentos():
     documentos_data = [
         {
             'nombre_documento': 'documento1',
             'estado_id': 1,
-            'legajo_id': 1
+            'legajo_id': 1,
+            "tipo_id": 1
         },
         {
             'nombre_documento': 'documento2',
             'estado_id': 2,
-            'legajo_id': 1
+            'legajo_id': 1,
+            "tipo_id": 1
         },
         {
             'nombre_documento': 'documento3',
             'estado_id': 3,
-            'legajo_id': 1
+            'legajo_id': 1,
+            "tipo_id": 1
         },
         {
             'nombre_documento': 'documento4',
             'estado_id': 4,
-            'legajo_id': 1
+            'legajo_id': 1,
+            "tipo_id": 1
         },
         {
             'nombre_documento': 'documento5',
             'estado_id': 2,
-            'legajo_id': 1
+            'legajo_id': 1,
+            "tipo_id": 1
         }
     ]
 

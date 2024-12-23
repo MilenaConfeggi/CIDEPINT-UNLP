4bdfeb5504fb6db28cd274c430d369eda476e7b1
Lautaro Gubia <gubia1263@gmail.com>
2024-12-23 20:11:33 -0300

    pendientes de informes y interarea

--- servicios/frontend/frontend/src/components/pendientes/DocumentosPendientes.vue
+++ servicios/frontend/frontend/src/components/pendientes/DocumentosPendientes.vue
@@ -1,3 +1,99 @@
 <template>
-    hola
+    <div class="container my-5">
+        <div v-if="informes.length > 0">
+            <h1 class="text-center">Informes pendientes</h1>
+            <div v-for="informe in informes" :key="informe.id" class="card mb-3 shadow-sm informe-card">
+                <div class="card-body d-flex align-items-center">
+                    <div class="left-content me-auto">
+                        <h5 class="card-title mb-1">Informe: {{ informe.nombre_documento }}</h5>
+                        <p class="card-text">Fecha: {{ formatFecha(informe.fecha_creacion) }}</p>
+                        <p class="card-text">Legajo: {{ informe.legajo_id }}</p>
+                        <p class="card-text">Cliente: {{ informe.legajo.cliente.contacto }}</p>
+                    </div>
+                    <div class="button-group">
+                        <RouterLink :to="`/legajos/${informe.legajo_id}`" class="btn btn-success">Ver</RouterLink>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div v-else>
+            <h1 class="text-center">No hay informes pendientes</h1>
+        </div>
+    </div>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            informes: [],
+            userRole: JSON.parse(localStorage.getItem('permisos')) || [],
+            area: JSON.parse(localStorage.getItem('area')) || 0,
+        };
+    },
+    methods: {
+        async fetchInformes() {
+            try {
+                const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/documentos/list/4`);
+                if (this.userRole.includes('cargar_informe_firmado')) {
+                    this.informes = response.data.filter(informe => informe.estado.id === 5);
+                }
+            } catch (error) {
+                console.error("Error al obtener los informes:", error);
+            }
+        },
+        formatFecha(fecha) {
+            const date = new Date(fecha);
+            const year = date.getFullYear();
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const day = String(date.getDate()).padStart(2, '0');
+            return `${year}-${month}-${day}`;
+        },
+    },
+    mounted() {
+        this.fetchInformes();
+    },
+};
+</script>
+
+<style scoped>
+.informe-card{
+    border-radius: 10px;
+    max-width: 600px;
+    margin: 0 auto;
+    height: 160px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between; 
+}
+
+.card-body {
+    padding: 15px;
+    display: flex;
+    justify-content: space-between;
+}
+
+.left-content {
+    text-align: left;
+}
+
+.card-title {
+    font-size: 1.25rem;
+    font-weight: bold;
+}
+
+.card-text {
+    margin-bottom: 10px;
+}
+
+.button-group {
+    display: flex;
+    align-items: center;
+}
+
+.btn-success {
+    margin-right: 10px;
+}
+</style>
--- servicios/frontend/frontend/src/components/pendientes/InterareasPendientes.vue
+++ servicios/frontend/frontend/src/components/pendientes/InterareasPendientes.vue
@@ -1,112 +1,100 @@
 <template>
-    <div class="container my-5">
-        <div v-if="interareas.length === 0" class="no-interareas text-center">
-            <h3>No hay interareas para mostrar</h3>
-        </div>
-        <div v-else>
-            <h1 v-if="userRole.includes('cargar_interarea_firmada')" class="text-center">Interáreas por firmar</h1>
-            <h1 v-else class="text-center">Interáreas por cargar</h1>
-            <div v-for="interarea in interareas" :key="interarea.id" class="card mb-3 shadow-sm">
-                <div class="card-body">
-                    <div>
-                        <h5 class="card-title mb-1">Interarea: {{ interarea.nro_interarea }}</h5>
-                        <p class="card-text">Fecha: {{ interarea.fecha_creacion }}</p>
-                        <p class="card-text">Área solicitante: {{ interarea.area_solicitante.nombre }}</p>
-                    </div>
-                    <div class="button-group">
-                        <RouterLink :to="`/interarea-detalle/${interarea.id}`" class="btn btn-success">Ver</RouterLink>
-                        <button class="btn btn-danger">b</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+  <div class="container my-5">
+    <div v-if="interareas.length === 0" class="no-interareas text-center">
+      <h3>No hay interareas para mostrar</h3>
     </div>
+    <div v-else>
+      <h1 v-if="userRole.includes('cargar_interarea_firmada')" class="text-center">Interáreas por firmar</h1>
+      <h1 v-else class="text-center">Interáreas por cargar</h1>
+      <div v-for="interarea in interareas" :key="interarea.id" class="card mb-3 shadow-sm interarea-card">
+        <div class="card-body d-flex align-items-center">
+          <div class="left-content me-auto">
+            <h5 class="card-title mb-1">Interarea: {{ interarea.nro_interarea }}</h5>
+            <p class="card-text">Fecha: {{ interarea.fecha_creacion }}</p>
+            <p class="card-text">Área solicitante: {{ interarea.area_solicitante.nombre }}</p>
+          </div>
+          <div class="button-group">
+            <RouterLink :to="`/interarea-detalle/${interarea.id}`" class="btn btn-success">Ver</RouterLink>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        interareas: [],
-        userRole: JSON.parse(localStorage.getItem('permisos')) || [],
-        area: JSON.parse(localStorage.getItem('area')) || 0,
-      };
-    },
-    methods: {
-      async fetchInterareas() {
-        try {
-          const status = this.userRole.includes('cargar_interarea_firmada') ? 1 : 2;
-          const response = await axios.get(`${import.meta.env.VITE_API_URL}/interareas`);
-  
-          if (status === 1) {
-            this.interareas = response.data.filter((interarea) => interarea.estadoInterarea_id === 1);
-          } else {
-            this.interareas = response.data.filter((interarea) => interarea.area_receptora.id === this.area && interarea.estadoInterarea_id === 2);
-          }
-        } catch (error) {
-          console.error("Error al obtener las interareas:", error);
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      interareas: [],
+      userRole: JSON.parse(localStorage.getItem('permisos')) || [],
+      area: JSON.parse(localStorage.getItem('area')) || 0,
+    };
+  },
+  methods: {
+    async fetchInterareas() {
+      try {
+        const status = this.userRole.includes('cargar_interarea_firmada') ? 1 : 2;
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/interareas`);
+
+        if (status === 1) {
+          this.interareas = response.data.filter((interarea) => interarea.estadoInterarea_id === 1);
+        } else {
+          this.interareas = response.data.filter((interarea) => interarea.area_receptora.id === this.area && interarea.estadoInterarea_id === 2);
         }
-      },
+      } catch (error) {
+        console.error("Error al obtener las interareas:", error);
+      }
     },
-    mounted() {
-      this.fetchInterareas();
-    },
-  };
-  </script>
-  
+  },
+  mounted() {
+    this.fetchInterareas();
+  },
+};
+</script>
+
 <style scoped>
-h1 {
-    font-size: 2.5rem;
-    font-weight: 600;
-    margin-bottom: 20px;
-    color: #343a40;
-}
-
 .no-interareas {
-    margin-top: 50px;
+  margin-top: 50px;
 }
 
-.card {
-    border-radius: 10px;
+.interarea-card {
+  border-radius: 10px;
+  max-width: 600px;
+  margin: 0 auto;
+  height: 160px; 
+  display: flex;
+  flex-direction: column; 
+  justify-content: space-between; 
 }
 
 .card-body {
-    padding: 20px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  padding: 15px;
+  display: flex;
+  justify-content: space-between;
+}
+
+.left-content {
+  text-align: left;
 }
 
 .card-title {
-    font-size: 1.25rem;
-    font-weight: bold;
-    margin-bottom: 5px;
+  font-size: 1.25rem;
+  font-weight: bold;
 }
 
 .card-text {
-    margin-bottom: 5px;
-    align-items: left;
+  margin-bottom: 10px;
 }
 
 .button-group {
-    display: flex;
-    gap: 10px;
+  display: flex;
+  align-items: center;
 }
 
 .btn-success {
-    margin-right: 10px;
-}
-
-.btn-danger {
-    background-color: #dc3545;
-    border-color: #dc3545;
-}
-
-.btn-danger:hover {
-    background-color: #c82333;
-    border-color: #bd2130;
+  margin-right: 10px;
 }
 </style>
-  

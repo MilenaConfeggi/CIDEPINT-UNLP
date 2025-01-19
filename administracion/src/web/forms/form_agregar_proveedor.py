90e1f171a80e5edf27b052aad9d41407874a2eb7
TOMAS IAGO BRUSCHI <tomasibruschi@hotmail.com.ar>
2025-01-19 18:07:38 -0300

    cambios finales que me olvide de subir :( (contacto es string y ahora compras si descarga un excel y no un csv)

--- administracion/src/web/forms/form_agregar_proveedor.py
+++ administracion/src/web/forms/form_agregar_proveedor.py
@@ -5,12 +5,7 @@ import re
 
 class form_agregar_proveedor(FlaskForm):
 
-    def validar_telefono(self, field):
-        telefono = str(field.data)
-        if not re.match(r'^\d{8,15}$', telefono):
-            raise ValidationError("El número de contacto debe tener entre 8 y 15 dígitos.")
-
     razon_social = StringField('razon social', validators=[DataRequired(
         message="Este campo es requerido"), Length(max=50)])
-    contacto = IntegerField('contacto', validators=[DataRequired(
-        message="Este campo es requerido"), validar_telefono])
+    contacto = StringField('contacto', validators=[DataRequired(
+        message="Este campo es requerido")])

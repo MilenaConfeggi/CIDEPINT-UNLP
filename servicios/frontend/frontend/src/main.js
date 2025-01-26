7ad3c198df7a0bf8d7e3fbe8390d0bfc32840af2
Lautaro Gubia <gubia1263@gmail.com>
2025-01-26 17:56:09 -0300

    arreglo rutas

--- servicios/frontend/frontend/src/main.js
+++ servicios/frontend/frontend/src/main.js
@@ -1,6 +1,4 @@
-import './assets/main.css'
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'bootstrap';
+import './assets/tipografia.css';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
